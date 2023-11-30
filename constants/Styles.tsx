200f7bdbc6986f574bc09420b31ae0d0b8620fc0
Teodoro Pacetti <teodoro.pacetti@studenti.unimi.it>
2023-11-30 22:04:48 +0100

    Created constants and helpers folders, moved styles

---
+++ constants/Styles.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	typeBadge: {
+		backgroundColor: "#9AF0FF",
+		borderRadius: 10,
+		marginHorizontal: 2,
+		marginTop: 5,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	typeFont: {
+		fontSize: 20,
+		color: "white",
+		padding: 3,
+	},
+	nameStyle: {
+		fontSize: 30,
+		paddingTop: 30,
+	},
+	idStyle: {
+		fontSize: 20,
+		paddingTop: 10,
+	},
+	genStyle: {
+		padding: 10,
+		fontSize: 40,
+		paddingTop: 20,
+	},
+});
